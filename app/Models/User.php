0724160f7dac7c14a0c1c986135948e696b08815
tadat216 <tadat216@gmail.com>
2024-11-04 02:43:44 +0700

    Revert "."
    This reverts commit cf711ca5c26137a09adea60f2092329cac30dfe6.

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,70 +1,49 @@
 <?php
 
-/**
- * Created by Reliese Model.
- */
-
 namespace App\Models;
 
-use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
-/**
- * Class User
- * 
- * @property int $id
- * @property int $plan_id
- * @property string $name
- * @property string $email
- * @property Carbon|null $email_verified_at
- * @property string $password
- * @property string|null $remember_token
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * 
- * @property Plan $plan
- * @property Collection|PaymentStatus[] $payment_statuses
- * @property Collection|Result[] $results
- *
- * @package App\Models
- */
-class User extends Model
+class User extends Authenticatable
 {
-	protected $connection = 'mysql';
-	protected $table = 'users';
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable, HasRoles;
 
-	protected $casts = [
-		'plan_id' => 'int',
-		'email_verified_at' => 'datetime'
-	];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
-	protected $hidden = [
-		'password',
-		'remember_token'
-	];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
-	protected $fillable = [
-		'plan_id',
-		'name',
-		'email',
-		'email_verified_at',
-		'password',
-		'remember_token'
-	];
-
-	public function plan()
-	{
-		return $this->belongsTo(Plan::class);
-	}
-
-	public function payment_statuses()
-	{
-		return $this->hasMany(PaymentStatus::class);
-	}
-
-	public function results()
-	{
-		return $this->hasMany(Result::class);
-	}
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
