f345f03bfe68bd499e556cf4dcbeb8b862a1ae28
huynhtuyet0201032019@gmail.com <huynhtuyet0201032019@gmail.com>
2024-11-05 20:44:53 +0700

    .

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
 
 /**
  * Class User
@@ -31,6 +32,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class User extends Model
 {
+	use HasRoles;
 	protected $connection = 'mysql';
 	protected $table = 'users';
 
